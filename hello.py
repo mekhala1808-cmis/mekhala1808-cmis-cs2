8dbbd435d825b241632e55e0979c8ded8a00ee2c
cmis <cmis@cmis-hs18.(none)>
2016-01-28 14:35:17 +0700

    Variables and random words

---
+++ hello.py
@@ -0,0 +1,14 @@
+print "Hello, world"
+print "My name is Mekhala"
+print "I like cotton candy"
+print "BAYUM"
+myName= "Mekhala"
+print myName
+print 3
+print "I reall like gummybears"
+print "I meant to type really"
+print True
+print 15
+myAge= 15
+myGrade= 10
+gradGrade=12
