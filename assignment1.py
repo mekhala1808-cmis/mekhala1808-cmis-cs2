af2e3a45cdd27e1f3ada836944d5780069117a50
cmis <cmis@cmis-hs17.(none)>
2016-02-01 09:56:20 +0700

    python

---
+++ assignment1.py
@@ -0,0 +1,11 @@
+myName = "Mekhala"
+myageInYears = 15
+myheightInMeters = 1.6
+lengthofonesideofasquareInInches = 5
+lengthofarectangleInInches = 10
+heightoftherectangleInInches = 6
+print myageInYears * 12
+myageInMonths = 180
+print 95 - myageInYears
+numberofyearsihavetoliveInYears = 80
+print float(myheightInMeters) * 3.3
