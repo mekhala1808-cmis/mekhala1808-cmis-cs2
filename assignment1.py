5675820b5f649e20f5987746baf668d2b7e7df7b
cmis <cmis@cmis-hs01.(none)>
2016-02-02 13:16:14 +0700

    assignment 1

--- assignment1.py
+++ assignment1.py
@@ -1,24 +1,42 @@
+# I created a variable and defined my name.
 myName = "Mekhala"
+# I created a variable and defined my age.
 myageInYears = 15
+# I created a variable and defined my height in meters.
 myheightInMeters = 1.6
+# I created a varibale and defined the length of side of one square in inches.
 lengthofonesideofasquareInInches = 5
+# I created a varibale and defined the length of a rectangle in inches.
 lengthofarectangleInInches = 10
+# I created a varibale and defined the height of a reactangle in inches. 
 heightoftherectangleInInches = 6
+# I created a varibale and defined my age in months.
 myageInMonths = myageInYears * 12
+# I created a varibale and defined the number of years I have left to live.
 numberofyearsihavetoliveInYears = 95 - myageInYears
+# I created a varibale and defined  my height in feet.
 myhieghtInFeet = float(myheightInMeters) * 3.3
+# I created a varibale and defined the average height of a female from my country in meters.
 averageheightInMeters = 1.7
+# I created a varibale and defined the difference between the average height of a female and my height in meters. 
 differencebetweenaverageandmyheight = averageheightInMeters - myheightInMeters
+# I created a varibale and defined the area of square.
 areaofasquare = lengthofonesideofasquareInInches * lengthofonesideofasquareInInches
+# I created a varibale and defined the volume of a cube.
 volumeofacube = 0.5 * 5 ** 3
+# I created a varibale and defined one ninth the area of the rectangle.
 oneninththeareaofareactangle = float(lengthofarectangleInInches) * heightoftherectangleInInches * 1/9
 print "I'm " + str(myName) + "."
 print "I'm " + str(myageInYears) + "."
 print "I'm " + str(myheightInMeters) + " meters tall" + "."
 print "I have " + str(numberofyearsihavetoliveInYears) + " years to live" + "."
 print "I'm " + str(differencebetweenaverageandmyheight) + " shorter than the average female in my country" + "."
-print "A square has a side length of ", lengthofonesideofasquareInInches, "."
-print "It has an area of ", areaofasquare, "."
-print "There's a rectangle with a length of ", lengthofarectangleInInches, "."
-print "The rectangle has a height of ", heightoftherectangleInInches, "."
+print "A square has a side length of ", lengthofonesideofasquareInInches,"."
+print "It has an area of ", areaofasquare,"."
+print "There's a rectangle with a length of ", lengthofarectangleInInches,"."
+print "The rectangle has a height of ", heightoftherectangleInInches,"."
+print "One ninth of the area of the rectangle is ",oneninththeareaofareactangle,"."
+winkyface = " ;) "
+print winkyface * 10000
+
 
