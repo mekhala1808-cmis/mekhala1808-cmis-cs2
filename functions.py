bc70aef5f37c8bed2f3bc833234ff4b30ad5bdde
cmis <cmis@cmis-hs17.(none)>
2016-02-11 09:53:00 +0700

    the rest of the stuff

--- functions.py
+++ functions.py
@@ -1,9 +1,31 @@
-def add(a,b):
-	return a+b
-c = add(2,3)
-print add(a,b)
-def sub(d,e):
-	return d-e
-f = sub(5,3)
-print sub(d-e)
+def add(a,b): #compute the sum of a and b
+	return a + b
+
+def sub(a,b): #compute the difference of a and b
+	return a - b
+
+def mul(a,b): #compute the product of a and b
+	return a * b
+
+def div(a,b): #compute the quotient of a and b
+	return a / b
+
+def hours_from_seconds(seconds): #convert seconds into hours 
+	return seconds/3600
+
+def circle_area(radius): #finds the area of a circle
+ 	return math.pi(radius)**2
+
+def sphere_volume(radius): #finds the volume of a sphere
+	return 4/3 * math.pi(radius)**3
+
+def avg_volume(a,b): #computes the average volume of two spheres
+	average = ((4/3 * math.pi(a)**3)+(4/3 * math.pi(b)**3))/2
+	return float(average)
+def area(a, b, c): #finds the area of a triangle using the lenth of three sides
+	p =(a+b+c)/1
+	return math.sqrt(p*(p-a)(p-b)(p-c))
+
+def right_allign(str"a"):
+	len(str
 
