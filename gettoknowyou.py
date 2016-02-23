e5d7e199ddf084e3d5d3a27af6b176b00ec59e04
cmis <cmis@cmis-hs17.(none)>
2016-02-23 14:10:19 +0700

    MORE

--- gettoknowyou.py
+++ gettoknowyou.py
@@ -1,21 +1,31 @@
-def add(x, y):
-	return x + y
-def output(name, x, y, z): 
+import math
+def mul(x, y):
+	return x * y
+
+def output(name, x, y, z, happy): 
 	out = """
-def main():
-name = raw input("What's your name?: ")
-x = raw_input("Type your lucky number: ")
-y= raw_input("Type your lucky number: ")
-happy= raw_input(What makes you happy?: ")
 
-z = int(x) + int(y)
-
-out = output(name)
-output = """
 Hey {}!
-Did you know:
-{} + {} = {}
+Did you know if your lucky number were the lenth of a rectangle and your unlucky number the width to find the volume of a rectangular prism would be to do:
+4 * ({} * {}) = {}
 I think you're cool, {}
+{} make me happy too!
+""".format(name, x, y, z, happy)
+	return out
 
-""".format(name, x, y, z, name)
-	return output
+def main():
+
+	name = raw_input("What's your name?: ")
+	x = raw_input("Type your lucky number: ")
+	y= raw_input("Type your unlucky number: ")
+	happy= raw_input("What makes you happy?: ")
+
+	z = int(x) / int(y)
+
+	out = output(name, x, y, z, happy)
+	print out
+
+main()
+
+
+
