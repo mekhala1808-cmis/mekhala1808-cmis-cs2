71797893646317535440b0451e6f4508e3db4a97
cmis <cmis@cmis-hs17.(none)>
2016-03-16 11:56:02 +0700

    no where near done

--- oneguess.py
+++ oneguess.py
@@ -4,18 +4,30 @@ import random
 minimumnumber= raw_input( "What is the minimum number?: ")
 maximumnumber= raw_input( "What is the maximum number?: ")
 
-print "I'm thinking of a number from " + str(minimumnumber) + " to " + str(maximumnumber) + "."
-
-targetnumber= raw_input( "What do you think it is?: ")
+targetnumber= random.random()
 
 def output(minimumnumber, maximumnumber, targetnumber):
-	out = """ """
+	out = """
+What is the minumum number? {}
+What is the maximum number? {}
+""".format(minimumnumber, maximumnumber)
 
-if targetnumber == True:
-    msg = "That's correct! You must be psychic!"
+
+
+def output(minimumnumber, maximumnumber):
+    out = """
+I'm thinkng of a number from {} to {}.
+""".format(minmumnumber, maximumnumber)
+
+
+
+guess = raw_input("What do you think it is?: ")
+
+if guessing == targetnumber
+    return = "That's correct! You must be psychic!"
 else:
-    msg = "
+    guessing == False 
+	return = "The target was random.random()"
 
-randomint(begginingnumber, endingnumber)
 
 
