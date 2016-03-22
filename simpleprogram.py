a23fb0378dc05ae2938cd57760dd4a172299cb84
cmis <cmis@cmis-hs07.(none)>
2016-03-22 09:53:33 +0700

    DONE

--- simpleprogram.py
+++ simpleprogram.py
@@ -1,14 +1,16 @@
-pythonimport math
-def mul(x, y):
-	return x * y
+import math
+
+def prism(x, y):
+    return 4 * (x * y)
+
 
 def output(name, x, y, z, happy): 
 	out = """
 
 Hey {}!
-Did you know if your lucky number were the lenth of a rectangle and your unlucky number the width to find the volume of a rectangular prism would be to do:
+Did you know that if your lucky number were the lenth of a rectangle and your unlucky number were the width, to find the volume of a rectangular prism would be to do:
 4 * ({} * {}) = {}
-I think you're cool, {}
+I think you're cool.
 {} make me happy too!
 """.format(name, x, y, z, happy)
 	return out
@@ -17,10 +19,10 @@ def main():
 
 	name = raw_input("What's your name?: ")
 	x = raw_input("Type your lucky number: ")
-	y= raw_input("Type your unlucky number: ")
-	happy= raw_input("What makes you happy?: ")
-
-	z = int(x) / int(y)
+	y = raw_input("Type your unlucky number: ")
+	happy = raw_input("What makes you happy?: ")
+    
+	z = prism(int(x), int(y))
 
 	out = output(name, x, y, z, happy)
 	print out
