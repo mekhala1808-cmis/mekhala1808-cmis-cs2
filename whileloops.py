1ccec4426c443a656ef30faf2c527b77e674cd24
cmis <cmis@cmis-hs22.(none)>
2016-05-24 09:52:54 +0700

    NU THANKS

---
+++ whileloops.py
@@ -0,0 +1,19 @@
+def countdown(x):
+    while x >= 0:
+        print x
+        x -= 1
+countdown(88)
+###
+def count(x):
+    if x > 0:
+        while x >= 0:
+            print x
+            x -= 1
+    else:
+        while x <= 0:
+            print x
+            x += 1
+count(4)
+count(-9)
+            
+
