e292d8cba52f985e56431c0450abf775945e3de8
cmis <cmis@cmis-hs22.(none)>
2016-05-26 14:37:01 +0700

    WOW

--- whileloops.py
+++ whileloops.py
@@ -1,4 +1,20 @@
-
+def countdown(x):
+    while x >= 0:
+        print x
+        x -= 1
+countdown(88)
+###
+def count(x):
+    if x > 0:
+        while x >= 0:
+            print x
+            x -= 1
+    else:
+        while x <= 0:
+            print x
+            x += 1
+count(4)
+count(-9)
 ###
 def countfrom(start, stop):
     while start < stop:
@@ -8,4 +24,13 @@ def countfrom(start, stop):
         print stop
         stop -= 1
 countfrom(-10, 10)
+###
+def sumofodds(x):
+    total = 0
+    while x < 0:
+        if x % == 0:
+            x += 1
+        else:
+            total += x
+            
 
