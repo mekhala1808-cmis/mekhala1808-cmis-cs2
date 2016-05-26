7f70980b1441dbf9dc83a8e70acfc70ff402055d
cmis <cmis@cmis-hs22.(none)>
2016-05-26 14:36:20 +0700

    WHAT

--- whileloops.py
+++ whileloops.py
@@ -1,19 +1,11 @@
-def countdown(x):
-    while x >= 0:
-        print x
-        x -= 1
-countdown(88)
+
 ###
-def count(x):
-    if x > 0:
-        while x >= 0:
-            print x
-            x -= 1
-    else:
-        while x <= 0:
-            print x
-            x += 1
-count(4)
-count(-9)
-            
+def countfrom(start, stop):
+    while start < stop:
+        print start
+        start += 1
+    while start > stop:
+        print stop
+        stop -= 1
+countfrom(-10, 10)
 
