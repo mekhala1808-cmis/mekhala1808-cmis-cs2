9ec83d8a1f38dbfaebc03cea76ce39483b71c828
cmis <cmis@cmis-hs10.(none)>
2016-06-01 10:24:34 +0700

    DONENOENOENOENE

--- recursiveguess.py
+++ recursiveguess.py
@@ -1,28 +1,30 @@
+
 import random
-def rounds(numberrounds, correct):
-    if numberrounds == 0:
-        return "You got " + str(correct) + "rounds correct."
-    else: 
-        print "Starting round " + str(numberrounds)
-        correct += guess(random.randint(1, 100), 5)
-        return rounds(numberrounds - 1, correct)
-def guess(random, attempt):
-    inp = int(raw_input("Guess a number: "))
-    if inp == random:
-        print "That's correct!"
-        return 1
-    elif attempts == 1:
-        print "The answer was " + str(random) + "You aren't that good at this game." 
-        return 0
-    elif inp > random:
-        print "That's too high."
-        return guess(random, attempt -1)
-    elif inp < random:
-        print "That's too low."
-        return guess(random, attempt -1)
-def main():
-    res = rounds(1, 0)
-    print res
+
+def rounds(numberrounds, c): 
+	if numberrounds == 0: 
+		return "You got " + str(c) + " rounds correct." 
+	else:
+		print "Starting round" + str(numberrounds)
+		c += guess(random.randint(1, 100), 6)
+		return rounds(numberrounds - 1, c)
+def guess(random, attempt): 
+	input = int(raw_input("Guess a number: "))
+	if input == random: 
+		print "That's correct!"
+		return 1 
+	elif attempt == 1: 
+		print "The answer was " + str(random) + " You aren't that good at this game"
+		return 0
+	elif input > random: 
+		print "That's too high." 
+		return guess(random, attempt -1) 
+	elif input < random: 
+		print "That's too low."
+		return guess(random, attempt -1)  
+def main(): 
+	result = rounds(1,0) 
+	print result
 main()
  
 
